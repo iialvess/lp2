fbb6658e2da7fcd14831407dd0a62e38da89a570
iialvess <iagoalvess011@gmail.com>
2026-09-22 14:45:58 -0300

    Crie a classe Aluno e o main(ControleAcademico)

---
+++ src/src/ControleAcademico.java
@@ -0,0 +1,8 @@
+public class ControleAcademico {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("JOAO", 1990);
+        Aluno a2 = new Aluno("MARIA", 2000);
+        System.out.println(a1);
+        System.out.println(a2);
+    }
+}
